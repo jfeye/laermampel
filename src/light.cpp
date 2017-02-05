8b94150b4c407dad38f1183af41af429960fab23
Janis Feye <janisfeye@web.de>
2017-02-05 16:08:35 +0100

    removed old code

--- src/light.cpp
+++ src/light.cpp
@@ -4,6 +4,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+#include "FastLED.h"
+
 #include "send_progmem.h"
 #include "html_index.h"
 
@@ -11,12 +13,14 @@
 #define HOSTNAME      "derhut" // mDNS-adress (http://HOSTNAME.local/)
 #define SSID          "Der Hut"
 #define PASSWD        "12341234"
-#define REGISTER_PIN  4
+#define LEDS_PIN 4
+#define NUM_LEDS 5
+#define MAX_CURRENT 500
+#define FRAMES_PER_SECOND 10
 
 ESP8266WebServer server(80);     // handles networking and provides http requests
-const uint16_t led[] = {D1,D2,D3,D4,D6};
 
-uint8_t numled;
+CRGB leds[NUM_LEDS];
 
 // Functions
 void serveIndex();
@@ -27,15 +31,13 @@ void handleRotate();
 void handleOther();
 
 void setup(void){
+
+  // LED setup
+  FastLED.addLeds<WS2812B, LEDS_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5,MAX_CURRENT);
+
   // Set up the debug connection
   Serial.begin(115200);
-
-  numled = sizeof(led)/sizeof(uint16_t); //number of leds
-  analogWriteRange(255);
-  for(int i=0; i<numled; i++){
-    pinMode(led[i], OUTPUT);
-    analogWrite(led[i], 255);
-  }
 
   WiFi.mode(WIFI_AP);
   WiFi.disconnect();
@@ -62,36 +64,12 @@ void setup(void){
   Serial.println("");
 }
 
-#define MODE_PLAIN 0
-#define MODE_ROTATE 1
-int mode = MODE_PLAIN;
-int frame = 0;
-
 void loop(void){
   server.handleClient();
-  delay(10);
-  //Serial.println("free heap:" + String(ESP.getFreeHeap()));
+  FastLED.delay(1000/FRAMES_PER_SECOND);
 
-  if (mode == MODE_ROTATE) {
-    frame++;
-    if (frame > 499) {
-      frame = 0;
-    }
-    for (int i = 0; i<5; i++) {
-      if (frame >= i*100 && frame < (i+1) * 100) {
-        int f = frame - i*100;
-        if (f < 30) {
-          analogWrite(led[i], (29-f) * 255 / 30);
-        } else if (f < 70) {
-          analogWrite(led[i], 0);
-        } else {
-          analogWrite(led[i], (f-70) * 255 / 30);
-        }
-      } else {
-        analogWrite(led[i], 255);
-      }
-    }
-  }
+  // TODO update leds
+
 }
 
 void serveIndex(){
@@ -116,39 +94,17 @@ void handleOther(){
 }
 
 void handleSet(){
-  mode = MODE_PLAIN;
-  uint8_t l = 255; //led index
-  uint8_t b = 255; //led brightness
-
-  for (uint8_t i=0; i < server.args(); i++) {
-    if(server.argName(i) == "led") l = atoi(server.arg(i).c_str()); //set next led index
-    else if(server.argName(i) == "b"){
-      b = atoi(server.arg(i).c_str());
-      if(l < numled) analogWrite(led[l], b);
-      l = 255;
-    }
-  }
-
   server.send ( 200, "text/plain", "OK" );
 }
 
 void handleOn(){
-  mode = MODE_PLAIN;
-  for(int i=0; i<numled; i++){
-    analogWrite(led[i], 255);
-  }
   server.send ( 200, "text/plain", "OK" );
 }
 
 void handleOff(){
-  mode = MODE_PLAIN;
-  for(int i=0; i<numled; i++){
-    analogWrite(led[i], 0);
-  }
   server.send ( 200, "text/plain", "OK" );
 }
 
 void handleRotate(){
-  mode = MODE_ROTATE;
   server.send ( 200, "text/plain", "OK" );
 }
