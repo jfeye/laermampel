258bbab86a8b2aa75f33532b00833ec9d32ad49e
Martin Blankenburg <martin.blankenburg@isp.uni-luebeck.de>
2017-02-05 20:43:08 +0100

    Website stuff

--- esp/src/light.cpp
+++ esp/src/light.cpp
@@ -4,8 +4,6 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-#include "FastLED.h"
-
 #include "send_progmem.h"
 #include "html_index.h"
 
@@ -13,39 +11,20 @@
 #define HOSTNAME      "laermampel" // mDNS-adress (http://HOSTNAME.local/)
 #define SSID          "Laermampel"
 #define PASSWD        "12341234"
-#define LEDS_PIN 4
-#define NUM_LEDS 5
-#define MAX_CURRENT 500
-#define FRAMES_PER_SECOND 30
-#define T1_BUFF_SIZE 200
-#define T2_BUFF_SIZE 5
 
 ESP8266WebServer server(80);     // handles networking and provides http requests
-
-CRGB leds[NUM_LEDS];
-uint8_t brightness = 255;
-uint8_t sensitivity = 128;
-
-uint32_t nextFrame = 0;
-
-uint16_t t1Buff[T1_BUFF_SIZE];
-uint16_t t1Buff_i = 0;
-uint16_t t2Buff[T2_BUFF_SIZE];
-uint16_t t2Buff_i = 0;
-
-double envbums = 0;
 
 // Functions
 void serveIndex();
 void handleSet();
 void handleOther();
-void setLEDs(uint8_t value);
+
+uint8_t brightness = 0;
+uint8_t sensitivity = 0;
 
 void setup(void){
-
-  pinMode(D1, OUTPUT);
   // Set up the debug connection
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   WiFi.mode(WIFI_AP);
   WiFi.disconnect();
@@ -57,7 +36,7 @@ void setup(void){
   server.begin();
 
   IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
+  Serial.print("\nAP IP address: ");
   Serial.println(myIP);
 
   // Set MDNS address ("hostname.local")
@@ -65,57 +44,11 @@ void setup(void){
     Serial.println("Fehler: MDNS fail");
   }
 
-  Serial.println("Setup finished!");
-  Serial.println("");
-
-  // LED setup
-  FastLED.addLeds<WS2812B, LEDS_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5,MAX_CURRENT);
+  Serial.println("Setup finished!\n");
 }
 
 void loop(void){
-  delayMicroseconds(100);
-  
-  uint16_t val = analogRead(A0);
-  if(val > 498) t1Buff[t1Buff_i] = val-498;
-  else t1Buff[t1Buff_i] = 498 - val;
-  t1Buff_i = (t1Buff_i+1);
-
-  if(t1Buff_i >= T1_BUFF_SIZE){
-    t1Buff_i = 0;
-    uint16_t max = 0;
-    for (uint16_t i = 0; i < T1_BUFF_SIZE; i++) {
-      if(t1Buff[i]> max)
-        max = t1Buff[i];
-    }
-    t2Buff[t2Buff_i] = max;
-    t2Buff_i = (t2Buff_i+1) % T2_BUFF_SIZE;
-  }
-
-  if(millis()>nextFrame){
-    server.handleClient();
-
-    uint16_t average = 0;
-    for (uint16_t i = 0; i < T2_BUFF_SIZE; i++){
-        average += t2Buff[i];
-    }
-    average /= T2_BUFF_SIZE;
-
-    if (average > envbums) envbums = average;
-    else if (average != envbums) envbums -= 0.5 + (envbums-average)/20.0;
-
-    if(envbums>255) envbums = 255;
-    setLEDs(envbums);
-    nextFrame = millis() + (1000/FRAMES_PER_SECOND);
-  }
-}
-
-void setLEDs(uint8_t value){
-  for(int i=0; i<NUM_LEDS; i++){
-    leds[i] = CHSV(96-(value*96)/255,255,255);
-  }
-  FastLED.setBrightness(brightness);
-  FastLED.show();
+  server.handleClient();
 }
 
 void serveIndex(){
@@ -123,7 +56,7 @@ void serveIndex(){
   sendProgmem(&server,html_index);
 }
 
-void handleOther(){
+void handleOther() {
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -135,26 +68,29 @@ void handleOther(){
   for ( uint8_t i = 0; i < server.args(); i++ ) {
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
-
   server.send ( 404, "text/plain", message );
 }
 
-void handleSet(){
+void handleSet() {
   uint8_t res = 0;
-
-  for(int i=0; i<server.args();i++){
-
-    if(server.argName(i) == "brightness") {
+  for (int i = 0; i < server.args(); i++) {
+    if (server.argName(i) == "brightness") {
       brightness = atoi(server.arg(i).c_str());
-    }else if(server.argName(i) == "sensitivity") {
+    } else if (server.argName(i) == "sensitivity") {
       sensitivity = atoi(server.arg(i).c_str());
-    }else{
+    } else {
         res = 1; // error arg not found
     }
     if (res != 0) {
       server.send(200, "text/html", "ERROR on " + server.argName(i) + " : " + server.arg(i));
     }
   }
-  if(res == 0 && server.args()>0) server.send(200, "text/html", "OK");
-  else server.send(404, "text/html", "ERROR");
+  if (res == 0 && server.args() > 0) {
+    Serial.write(0xff);
+    Serial.write(brightness);
+    Serial.write(sensitivity);
+    server.send(200, "text/html", "OK");
+  } else {
+    server.send(404, "text/html", "ERROR");
+  }
 }
